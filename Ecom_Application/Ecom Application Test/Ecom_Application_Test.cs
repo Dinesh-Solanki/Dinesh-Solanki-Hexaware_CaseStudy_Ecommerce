e00e3ecda7da792e94a5c3f0e1f69636e29227b6
Dinesh Solanki g <dineshsolankig6942@gmail.com>
2024-01-16 17:34:30 +0530

    Ecommerce

---
+++ Ecom_Application/Ecom Application Test/Ecom_Application_Test.cs
@@ -0,0 +1,78 @@
+using Ecom_Application.DAO;
+using Ecom_Application.Entity;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Ecom_Application_Test
+{
+    public class Ecom_Application_Testing
+    {
+        private OrderProcessorRepository _processorRepository;
+        [SetUp]
+        public void Setup()
+        {
+            AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", string.Format("{0}\\app.config", AppDomain.CurrentDomain.BaseDirectory));
+            _processorRepository = new OrderProcessorRepositoryImpl();
+        }
+
+
+        [Test]
+        public void CreateProduct_WhenPassed()
+        {
+            Products product = new Products() { Name = "Paracetamol", Price = 13.99M, Description = "Tablet", StockQuantity = 195 };
+            bool actual = _processorRepository.createProduct(product);
+            Assert.That(actual, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void AddedToCart_WhenPassed()
+        {
+            Customers customers = new Customers() { Customer_id = 4 };
+            Products product = new Products() { Product_id = 2 };
+            int quantity = 10;
+            bool addedtocart = _processorRepository.addToCart(customers, product, quantity);
+            Assert.That(addedtocart, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void AddedToCart_WhenFailed()
+        {
+            Customers customers = new Customers() { Customer_id = 20 };
+            Products product = new Products() { Product_id = 8 };
+            int quantity = 10;
+            bool addedtocart = _processorRepository.addToCart(customers, product, quantity);
+            Assert.That(addedtocart, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void OrderPlaced_WhenPassed() 
+        {
+            Customers customers = new Customers() { Customer_id = 1 };
+            Products products=new Products() { Product_id = 100 };
+            int quantity = 10;
+            List<Tuple<Products, int>> testinglist = new List<Tuple<Products, int>>();
+            testinglist.Add(Tuple.Create(products, quantity));
+            string adddress = "Bangalore";
+            bool placed=_processorRepository.PlaceOrder(customers, testinglist, adddress);
+            Assert.That(placed, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void OrderPlaced_WhenFailed()
+        {
+            Customers customers = new Customers() { Customer_id = 500 };
+            Products products = new Products() { Product_id = 2 };
+            int quantity = 10;
+            List<Tuple<Products, int>> testinglist = new List<Tuple<Products, int>>();
+            testinglist.Add(Tuple.Create(products, quantity));
+            string adddress = "Bangalore";
+            bool placed = _processorRepository.PlaceOrder(customers, testinglist, adddress);
+            Assert.That(placed, Is.EqualTo(false));
+        }
+
+
+
+
+
+    }
+}
